675116d0c6fcc76a275d82533795b3c9f28ab300
bahodurotayev <tjkbaha@gmail.com>
2023-01-06 16:29:44 -0500

    full fix on setUp

---
+++ src/day15/_02WaitExplicit.java
@@ -0,0 +1,31 @@
+package day15;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.locks.Condition;
+
+import static utils.BaseClass.*;
+public class _02WaitExplicit {
+    public static void main(String[] args) {
+        setUp("http://uitestpractice.com/Students/Contact");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(55));
+
+        try {
+            WebElement ajaxLink = driver.findElement(By.linkText("This is a Ajax link"));
+            ajaxLink.click();
+
+            //wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.ContactUs p")));
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div.ContactUs p")));
+
+            WebElement contactUsText = driver.findElement(By.cssSelector("div.ContactUs p"));
+            System.out.println(contactUsText.getText());
+        }catch (Exception exception){
+            System.out.println("Elements are not found");
+        }
+        tearDown();
+    }
+}
