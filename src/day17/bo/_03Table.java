10d26b6bd56d255fc1abff8b9a0e84eb358bd391
bahodurotayev <tjkbaha@gmail.com>
2023-01-19 01:28:21 -0500

    table

--- src/day17/bo/_03Table.java
+++ src/day17/bo/_03Table.java
@@ -26,7 +26,7 @@ public class _03Table {
         
         List<WebElement> tableCheckbox = driver.findElements(By.cssSelector("table.SampleTable tr td:nth-child(1)"));
         List<WebElement> tableName = driver.findElements(By.cssSelector("table.SampleTable tr td:nth-child(2)"));
-        System.out.println(tableName.size());
+        System.out.println("actual size of table = " + tableName.size());
         //td[text() = 'Paul Brown']//preceding-sibling::td//input
 
         for (int i = 0; i < tableName.size(); i++) {
@@ -39,7 +39,31 @@ public class _03Table {
         click_clickAbility(delete_button);
 
         List<WebElement> tableName2 = driver.findElements(By.cssSelector("table.SampleTable tr td:nth-child(2)"));
-        System.out.println(tableName2.size());
+        System.out.println("Current size of table = " + tableName2.size());
         tearDown();
     }
+    /* // 1st way: Ufuk Code:
+        List<WebElement> actualValues = driver.findElements(By.xpath("(//table[@class='SampleTable']//tr/td[2])"));
+        for (int i = 0; i < actualValues.size(); i++) {
+            if (actualValues.get(i).getText().equals(expected_Value)) {
+                driver.findElement(By.xpath("(//input[@type='checkbox'])[" + (i + 1) + "]")).click();    // +1 We need to skip header,
+                wait(1);
+                driver.findElement(By.cssSelector("#ctl00_MainContent_btnDelete")).click();
+                wait(1);
+                break;
+            }
+        }*/
+
+    // 2nd way: Dilnoza Code
+//        String expectedValue = "Clare Jefferson";
+//        List<WebElement> actualValues = driver.findElements(By.xpath("//table[@class='SampleTable']//tr/td[2]"));
+//        for (int i = 0; i < actualValues.size(); i++) {
+//            if (actualValues.get(i).getText().equals(expectedValue)) {
+//                driver.findElement(By.xpath("(//input[@type='checkbox'])[" + (i+1) + "]")).click();
+//                wait(1);
+//                driver.findElement(By.id("ctl00_MainContent_btnDelete")).click();//
+//                wait(1);
+//                break;
+//            }
+//        }
 }
---
+++ src/day17/bo/_04Table.java
@@ -0,0 +1,16 @@
+package day17.bo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static utils.BaseClass.*;
+import static utils.CommonMethods.click_clickAbility;
+import static utils.CommonMethods.send_Text;
+
+public class _04Table {
+    public static void main(String[] args) {
+        setUp("https://demoqa.com/webtables");
+    }
+}
