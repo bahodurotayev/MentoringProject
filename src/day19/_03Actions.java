f737c9b5edb3c02b20f871f6885b6538c161f7c2
bahodurotayev <tjkbaha@gmail.com>
2023-01-27 12:41:49 -0500

    screenshot in CommonMethods

--- src/day19/_03Actions.java
+++ src/day19/_03Actions.java
@@ -3,10 +3,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 import static utils.BaseClass.*;
 
 public class _03Actions {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AWTException {
         setUp("https://www.ebay.com");
 
         WebElement my_eBay = driver.findElement(By.linkText("My eBay"));
@@ -19,6 +22,19 @@ public class _03Actions {
         }else {
             System.out.println("Error!!!");
         }
+
+        WebElement electronics = driver.findElement(By.xpath("//li[@class='hl-cat-nav__js-tab'][2]"));
+        action.moveToElement(electronics).perform();
+        action.contextClick(electronics).perform();
+        Robot robot = new Robot();
+        robot.keyPress(KeyEvent.VK_ESCAPE);
+        waitSecond(1);
+        driver.navigate().to("https://demoqa.com/buttons");    // We are navigating from eBay to this new website
+        waitSecond(1);
+        // Here we are locating 'Double Click Me' button and using double click on it from the Actions Class.
+        action.doubleClick(driver.findElement(By.id("doubleClickBtn"))).perform();
+        System.out.println(driver.findElement(By.id("doubleClickMessage")).getText());     // Locating and Printing message for Double Click Me.
+        waitSecond(1);
 
     }
 }
